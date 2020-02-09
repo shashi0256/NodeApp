323277dbbee17c1faf50659c3fcffb58844d9ed3
shashidhar <priyankareddy@Priyankas-MacBook-Pro.local>
2020-02-09 10:49:35 -0600

    adding reddy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 //App
 const app = express();
 app.get('/', (req,res) => {
-  res.send('Hello shashidhar congratulations');
+  res.send('Hello shashidhar reddy congratulations');
 });
 
 app.listen(PORT, HOST);
